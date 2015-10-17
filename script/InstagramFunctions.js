860e0544a806bbfd44c0ef3e8d5ffc3f264df255
Miguel <miguellissimo@gmail.com>
2015-10-17 17:13:43 +0200

    instagram functions

--- script/InstagramFunctions.js
+++ script/InstagramFunctions.js
@@ -1,14 +1,25 @@
-var InstagramFunctions = (function () {
+var InstagramFunctions = (function (window, document, undefined) {
 
-	var ret = {}
+    var ret = {}
 
-	/*
-	TODO:
+    ret.get_image_links_for = function(latitude, longitude, callback) {
+        console.log("get_images_links_for called");
+        $.ajax({
+            url: 'https://api.instagram.com/v1/tags/' + hashtag + '/media/recent?client_id=' + clientId,
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function(data) {
+            var tuple_list = []; 
+            for (i = 0; i < n; i++) {
+                var link_profile = data.data[i].link;
+                var thumbnail_link = data.data[i].images.low_resolution.url;
+                tuple_list.push([link_profile, thumbnail_link]);
+            }
+            console.log(tuple_list); }
+        });
+    };
+    
+    return ret;
 
-	*/
-
-
-
-	return ret;
-
-})();
+})(window, document);
