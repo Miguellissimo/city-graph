0aca206597eb6ca8c537bdbdda81d233d32ecb31
Philip Dzierzon <home@pdzierzon.de>
2015-10-18 10:49:35 +0200

    no message

--- script/InstagramFunctions.js
+++ script/InstagramFunctions.js
@@ -8,14 +8,14 @@ var InstagramFunctions = (function (window, document, undefined) {
     var INSTAGRAMURL = "https://api.instagram.com/v1/tags/";
     var RECENTURLEXTENSION = "/media/recent?client_id=";
 
-    function getImagesFromUrl(url, callBack, count) {
+    function getImagesFromUrl(url, callBack) {
         $.ajax({
             url: url,
             type: 'GET',
             crossDomain: true,
             dataType: 'jsonp',
             success: function(data) {
-                callBack(data, count);
+                callBack(data);
             }
         });
     }
@@ -23,7 +23,7 @@ var InstagramFunctions = (function (window, document, undefined) {
     ret.getRecentImages = function(hashtag, n, lat, lng, callback_function) {
         console.log("getRecentImages called");
 
-        function callBack(data, count) {
+        function callBack(data) {
             var tuple_list = []; 
             for (var i = 0; i < n; i++) {
                 var link_profile = data.data[i].link;
@@ -35,52 +35,6 @@ var InstagramFunctions = (function (window, document, undefined) {
 
         var url = INSTAGRAMURL+ hashtag + RECENTURLEXTENSION + InstagramApi.clientId;
         getImagesFromUrl(url, callBack);
-    };
-
-    ret.getPopularImages = function(hashtag, n, callback_function) {
-        console.log("getPopularImages called");
-
-        var url = INSTAGRAMURL+ hashtag + RECENTURLEXTENSION + InstagramApi.clientId;
-        var images = [];
-
-        function callBack(data, count) {
-            if(count === 0) {
-                return;
-            }
-
-            url = data.pagination.next_url;
-            for(var i = 0; i < data.data.length; i++) {
-                var link_profile = data.data[i].link;
-                var thumbnail_link = data.data[i].images.thumbnail.url;
-                var likeCount = data.data[i].likes.count;
-                images.push([link_profile, thumbnail_link, likeCount]);
-            }
-
-            getImagesFromUrl(url, callBack, count-1);
-        }
-
-        getImagesFromUrl(url, callBack, 5);
-
-        images.sort(function(a , b) {
-            if(a[2] < b[2]) {
-                return 1;
-            }
-
-            if(a[2] > b[2]) {
-                return -1;
-            }
-
-            return 0;
-        });
-
-        var ret = [];
-
-        for(var i = 0; i < n; i++) {
-            ret.push(images[i]);
-        }
-
-        callback_function(ret);
-        
     };
     
     return ret;
